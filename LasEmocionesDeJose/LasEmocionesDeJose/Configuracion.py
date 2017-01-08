63d8aad1cb67b28bf5c038dea9da448a9b248e53
HaroldReyes <haroldthomasreyes@gmail.com>
2017-01-08 02:00:34 -0300

    Pantalla de inicio

---
+++ LasEmocionesDeJose/LasEmocionesDeJose/Configuracion.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+import random, pygame
+
+
+FPS_inicial = 3
+TIEMPO_MAX = 61
+
+ANCHO = 800
+ALTO = 600
+   
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255) 
+Red= 	(255,0,0)
+Lime	= 	(0,255,0)
+Blue= 	(0,0,255)
+Yellow	= 	(255,255,0)
+Aqua	= 	(0,255,255)
+Fuchsia	= 	(255,0,255)
+Silver	= 	(192,192,192)
+Gray	= 	(128,128,128)
+Maroon	= 	(128,0,0)
+Olive	= 	(128,128,0)
+Green	= 	(0,128,0)
+Purple	= 	(128,0,128)
+Teal= 	(0,128,128)
+Navy	= 	(0,0,128)
+
+TAMANNO_TITULOS = 56
+TAMANNO_LETRA = 36
+F_AarvarkCafe = 'Fuentes/Aarvark Cafe.ttf'
+
+Dimensiones = [ANCHO,ALTO]
+Fondo_Intro='Imagenes/BackGround_Intro.png'
--- LasEmocionesDeJose/LasEmocionesDeJose/LasEmocionesDeJose.py
+++ LasEmocionesDeJose/LasEmocionesDeJose/LasEmocionesDeJose.py
@@ -1,75 +1,44 @@
-"""
- Ejemplo de cómo crear una ventana de instrucciones.
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
-"""
-import pygame
+import pygame, os,  sys
+from Configuracion  import *
+from Funciones  import *
  
-# Definimos algunos colores
-NEGRO = (0, 0, 0)
-BLANCO = (255, 255, 255)
- 
-pygame.init()
-  
-#  Establecemos el largo y ancho de la pantalla.
-dimensiones = [700,500]
-pantalla = pygame.display.set_mode(dimensiones)
- 
-pygame.display.set_caption("Pantalla de Instrucciones")
- 
-#Iteramos hasta que el usuario haga click sobre le botón de salida.
-hecho = False
- 
-#  Usado para gestionar cuán rápido se actualiza la pantalla
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+pygame.init() 
+#pantalla = pygame.display.set_mode(Dimensiones, pygame.FULLSCREEN) 
+pantalla = pygame.display.set_mode(Dimensiones) 
+pygame.display.set_caption("A Jugar")
 reloj = pygame.time.Clock()
+
+hecho = False 
+Mostrar_Inicio = True
+Pagina_de_Inicio = 1
  
-# Posición de partida del rectángulo
-rect_x = 50
-rect_y = 50
- 
-# Velocidad y dirección del rectángulo
-rect_cambio_x = 5
-rect_cambio_y = 5
- 
-# Esta es la fuente que usaremos para el texto que aparecerá en pantalla (tamaño 36)
-fuente = pygame.font.Font(None, 36)
- 
-mostrar_instrucciones = True
-pagina_de_instrucciones = 1
- 
-# -------- Bucle de la Página de Instrucciones -----------
-while not hecho and mostrar_instrucciones:
+Fuente1 = pygame.font.Font(F_AarvarkCafe, TAMANNO_TITULOS) 
+Background_Inicio=pygame.image.load(Fondo_Intro).convert()
+
+# -------- Bucle de la Pagina de Inicio -----------
+while not hecho and Mostrar_Inicio:
     for evento in pygame.event.get(): #  El usuario hizo algo
         if evento.type == pygame.QUIT: # Si el usuario hace click sobre cerrar
             hecho = True # Marca que ya lo hemos hecho, de forma que abandonamos el bucle
         if evento.type == pygame.MOUSEBUTTONDOWN:
-            pagina_de_instrucciones += 1
-            if pagina_de_instrucciones == 3:
-                mostrar_instrucciones = False
+            Pagina_de_Inicio += 1
+            if Pagina_de_Inicio == 2:
+                Mostrar_Inicio = False
  
     # Limpia la pantalla y establece su color de fondo
     pantalla.fill(NEGRO)
  
-    if pagina_de_instrucciones == 1:
-        # Instrucciones de dibujo, página 1
-        # Esto también podría cargar una imagen realizada por cualquier otro programa.
-        # De esta forma sería más fácil y flexible.
-         
-        texto = fuente.render("Instrucciones", True, BLANCO)
+    if Pagina_de_Inicio == 1:
+        # Instrucciones de dibujo, pagina 1
+        # Esto tambien podria cargar una imagen realizada por cualquier otro programa.
+        # De esta forma seria mï¿½s facil y flexible.
+          
+        pantalla.blit(Background_Inicio,(0,0))
+        texto = Fuente1.render("Las emociones de Jose", True, Teal)
         pantalla.blit(texto, [10, 10])
          
-        texto = fuente.render("Página 1", True, BLANCO)
-        pantalla.blit(texto, [10, 40])
-         
-    if pagina_de_instrucciones == 2:
-        # Instrucciones de dibujo, página 2
-        texto = fuente.render("Este programa hace rebotar un rectángulo", True, BLANCO)
-        pantalla.blit(texto, [10, 10])
-         
-        texto = fuente.render("Página 2", True, BLANCO)
-        pantalla.blit(texto, [10, 40])
+        
     # Limitamos a 20 fotogramas por segundo.
     reloj.tick(20)
  
@@ -84,26 +53,13 @@ while not hecho:
  
     # Limpia la pantalla y establece su color de fondo
     pantalla.fill(NEGRO)
- 
-    # Dibuja el rectángulo
-    pygame.draw.rect(pantalla,BLANCO,[rect_x, rect_y, 50, 50])
   
-    # Mueve el punto de partida del rectángulo      
-    rect_x += rect_cambio_x
-    rect_y += rect_cambio_y
- 
-    # Rebota el rectángulo, si hace falta.
-    if rect_y > 450 or rect_y < 0:
-        rect_cambio_y = rect_cambio_y * -1
-    if rect_x > 650 or rect_x < 0:
-        rect_cambio_x = rect_cambio_x * -1
      
     # Limitamos a 60 fotogramas por segundo.
-    reloj.tick(60)
- 
+    reloj.tick(60) 
     # Avancemos y actualicemos la pantalla con lo que hemos dibujado.
     pygame.display.flip()
      
-# Pórtate bien con el IDLE. Si nos olvidamos de esta línea, el programa se 'colgará'
+# Partate bien con el IDLE. Si nos olvidamos de esta linea, el programa se 'colgara'
 # en la salida.
 pygame.quit()
